d89dda92e98a811efde535559ff6dcdd0fce46ac
Daniel <danielgraogg@gmail.com>
2025-03-01 15:30:04 +0100

    Added Home Page Scaffolding, and Modified structure to compyl with professor demands

--- app/src/main/java/com/example/travelplanner/MainActivity.kt
+++ app/src/main/java/com/example/travelplanner/MainActivity.kt
@@ -4,26 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.travelplanner.ui.theme.TravelPlannerTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.travelplanner.ui.theme.TestingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TravelPlannerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            TestingTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    MainScreen()
                 }
             }
         }
@@ -31,17 +25,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TravelPlannerTheme {
-        Greeting("Android")
-    }
+fun MainScreen() {
+    val navController = rememberNavController()
+    NavGraph(navController = navController)
 }
